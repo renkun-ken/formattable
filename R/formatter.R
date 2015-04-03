87f9932353c03b07719ecb4f41645e1570f90ee5
Kun Ren <renkun@outlook.com>
2015-04-03 22:57:21 +0800

    use identity by default if text formatter is missing

--- R/formatter.R
+++ R/formatter.R
@@ -1,6 +1,15 @@
 #' @export
 formatter <- function(tag, ...) {
   args <- list(...)
+
+  # if function to specify element inner text is missing,
+  # then use identify to preserve the default formatting of
+  # the column value
+  if(all(nzchar(names(args)))) {
+    args <- c(args, identity)
+  }
+
+  # create a closure for formattable to build output string
   function(x) {
     values <- lapply(args, function(arg) {
       if(is.function(arg)) arg(x) else arg
