7e4c15eff370ca07495cb9e3c7b8b95bdf02cfec
Kun Ren <renkun@outlook.com>
2015-04-22 20:06:21 +0800

    formattable class

--- R/formattable.R
+++ R/formattable.R
@@ -4,9 +4,8 @@
 #' formatted table presented in a mix of markdown/reStructuredText and
 #' HTML elements. To generate a formatted table, each column of data
 #' frame can be transformed by formatter function.
-#' @param data data
-#' @param formatter A named list of formatter functions.
-#' @param ... additional parameters passed to \code{knitr::kable}
+#' @param x data
+#' @param ... additional parameters
 #' @export
 #' @return a \code{knitr_kable} object whose \code{print} method generates a
 #' string-representation of \code{data} formatted by \code{formatter} in specific \code{format}.
@@ -34,16 +33,49 @@
 #'    "padding-right" = "4px",
 #'    color = "white",
 #'    "background-color" = rgb(x/max(x), 0, 0)))))
-formattable <- function(data, formatter = list(), ...)
+formattable <- function(x, ...)
   UseMethod("formattable")
 
+#' @export
+formattable.default <- function(x, ..., prefix = "", suffix = "") {
+  if ("formattable" %in% (class <- class(x)))
+    return(x)
+  class(x) <- c("formattable", class)
+  attr(x, "format") <- list(...)
+  attr(x, "prefix") <- prefix
+  attr(x, "suffix") <- suffix
+  x
+}
+
+#' @export
+as.character.formattable <- function(x, ..., formatter = "identity",
+  justify = "none", na.encode = FALSE) {
+  format_args <- attr(x, "format", exact = TRUE)
+  custom_args <- list(...)
+  format_args[names(custom_args)] <- custom_args
+  str <- do.call(formatC, c(list(unclass(x)), format_args))
+  paste0(ifelse(is.na(x), "", attr(x, "prefix", exact = TRUE)), str,
+    ifelse(is.na(x), "", attr(x, "suffix", exact = TRUE)))
+}
+
+#' @export
+print.formattable <- function(x, ...) {
+  print(as.character.formattable(x), ..., quote = is.character(x))
+}
+
+#' @export
+format.formattable <- function(x, ...) {
+  as.character.formattable(x, ...)
+}
+
 #' @rdname formattable
+#' @param formatter A named list of formatter functions.
 #' @param format The output format: markdown or pandoc?
 #' @param align The alignment of columns: a character vector consisting of \code{'l'} (left),
 #' \code{'c'} (center), and/or \code{'r'} (right). By default, all columns are right-aligned.
 #' @param digits An integer that all numeric columns are rounded to.
 #' @export
-formattable.data.frame <- function(data, formatter = list(),
+formattable.data.frame <- function(x, formatter = list(),
   format = c("markdown", "pandoc"), align = "r", digits = getOption("digits"), ...) {
   format <- match.arg(format)
   envir <- parent.frame()
@@ -61,13 +93,12 @@ formattable.data.frame <- function(data, formatter = list(),
       f(x)
     }
     as.character(value)
-  }, data, names(data), SIMPLIFY = FALSE),
-    row.names = row.names(data), stringsAsFactors = FALSE)
+  }, x, names(x), SIMPLIFY = FALSE),
+    row.names = row.names(x), stringsAsFactors = FALSE)
   knitr::kable(xdf, format = format, align = align, escape = FALSE, ...)
 }
 
-#' @rdname formattable
 #' @export
-formattable.matrix <- function(data, formatter = list(), ...) {
-  formattable.data.frame(data.frame(data, stringsAsFactors = FALSE), formatter = formatter, ...)
+formattable.matrix <- function(x, formatter = list(), ...) {
+  formattable.data.frame(data.frame(x, stringsAsFactors = FALSE), formatter = formatter, ...)
 }
