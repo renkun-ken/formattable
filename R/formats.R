675d38b272aecbf5dc5bf7ac79055fc47564c012
Kun Ren <renkun@outlook.com>
2015-04-25 21:35:29 +0800

    fix incorrect params

--- R/formats.R
+++ R/formats.R
@@ -1,9 +1,9 @@
 #' Percentage representation of numeric values
 #'
 #' @param x a numeric vector.
-#' @param digits an integer to indicate the number of digits of the percentage string.
 #' @param format format type passed to \code{\link{formatC}}.
-#' @param ... Additional parameters passed to \code{formatC}.
+#' @param digits an integer to indicate the number of digits of the percentage string.
+#' @param ... additional parameters passed to \code{formattable}.
 #' @export
 #' @examples
 #' percent(rnorm(10, 0, 0.1))
@@ -15,8 +15,8 @@ percent <- function(x, format = "f", digits = 2L, ...) {
 }
 
 #' @export
-decimal <- function(x, format = "f", big.mark = ",", ...) {
-  formattable(x, format = "f", big.mark = big.mark, ...)
+decimal <- function(x, format = "f", big.mark = ",", digits = 2L, ...) {
+  formattable(x, format = format, big.mark = big.mark, digits = 2L, ...)
 }
 
 #' @export
