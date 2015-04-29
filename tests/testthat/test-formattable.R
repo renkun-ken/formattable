36078809d31c80a004049123a9ceb608feb0ac1d
Kun Ren <renkun@outlook.com>
2015-04-29 17:05:35 +0800

    fix class test of formattable

--- tests/testthat/test-formattable.R
+++ tests/testthat/test-formattable.R
@@ -1,5 +1,6 @@
 context("formattable")
 
 test_that("formattable", {
-  expect_is(formattable(mtcars), "knitr_kable")
+  expect_is(formattable(mtcars), "formattable")
+  expect_is(format_table(formattable(mtcars)), "knitr_kable")
 })
