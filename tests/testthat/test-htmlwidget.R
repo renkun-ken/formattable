3849ecb41dd20c6761a716c5245ec3f1610b5d18
Kun Ren <renkun@outlook.com>
2015-06-11 23:50:23 +0800

    add simple type-checking for htmlwidget

--- tests/testthat/test-htmlwidget.R
+++ tests/testthat/test-htmlwidget.R
@@ -1,8 +1,6 @@
 context("htmlwidget")
 
-library(htmlwidgets)
-
 test_that("conversion", {
-  expect_is( as.htmlwidget(formattable(mtcars)), "formattable_widget" )
-  expect_is( as.htmlwidget(formattable(mtcars)), "htmlwidget" )
+  expect_is(as.htmlwidget(formattable(mtcars)), "formattable_widget")
+  expect_is(as.htmlwidget(formattable(mtcars)), "htmlwidget")
 })
